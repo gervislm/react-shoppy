fdb029191c82230bcc22dfc623408fdd4c60320f
Gervis Maldonado <gervislm@icloud.com>
2023-07-20 12:21:41 -0400

    react router y rutas creadas

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,7 +1,31 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+import Home from '../Home';
+import MyAccount from '../MyAccount';
+import MyOrder from '../MyOrder';
+import MyOrders from '../MyOrders';
+import NotFound from '../NotFound';
+import SingIn from '../SingIn';
 import './App.css';
 
-function App() {
-  return <div className='bg-red-100'>Hola mundo bebe</div>;
-}
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sing-in', element: <SingIn /> },
+    { path: '/*', element: <NotFound /> },
+  ]);
+
+  return routes;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+};
 
 export default App;
