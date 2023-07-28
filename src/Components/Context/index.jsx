5c361f69bf6793bf0842babf67230cd7565674c2
gervislm <gervislm@icloud.com>
2023-07-28 17:50:41 -0400

    Context Creado

--- src/Components/Context/index.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const ShoppingCartContext = createContext();
-
-export const ShoppingCartProvider = ({ children }) => {
-  return (
-    <ShoppingCartContext.Provider>{children}</ShoppingCartContext.Provider>
-  );
-};
